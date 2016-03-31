a48fd92c947715174e23cf144d8b5789b520510d
Gog <gogntp@gmail.com>
2016-03-31 15:06:21 +0400

    [upd] custom toolbar

--- src/draw/DrawToolbar.js
+++ src/draw/DrawToolbar.js
@@ -33,20 +33,20 @@ L.DrawToolbar = L.Toolbar.extend({
 				handler: new L.Draw.Polyline(map, this.options.polyline),
 				title: L.drawLocal.draw.toolbar.buttons.polyline
 			},
-			{
-				enabled: this.options.polygon,
-				handler: new L.Draw.Polygon(map, this.options.polygon),
-				title: L.drawLocal.draw.toolbar.buttons.polygon
-			},
-			{
-				enabled: this.options.rectangle,
-				handler: new L.Draw.Rectangle(map, this.options.rectangle),
-				title: L.drawLocal.draw.toolbar.buttons.rectangle
-			},
-			{
-				enabled: this.options.circle,
-				handler: new L.Draw.Circle(map, this.options.circle),
-				title: L.drawLocal.draw.toolbar.buttons.circle
+//			{
+//				enabled: this.options.polygon,
+//				handler: new L.Draw.Polygon(map, this.options.polygon),
+//				title: L.drawLocal.draw.toolbar.buttons.polygon
+//			},
+//			{
+//				enabled: this.options.rectangle,
+//				handler: new L.Draw.Rectangle(map, this.options.rectangle),
+//				title: L.drawLocal.draw.toolbar.buttons.rectangle
+//			},
+//			{
+//				enabled: this.options.circle,
+//				handler: new L.Draw.Circle(map, this.options.circle),
+//				title: L.drawLocal.draw.toolbar.buttons.circle
 			},
 			{
 				enabled: this.options.marker,
